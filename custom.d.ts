9cbb110eada23bfd58c79dcba01e3159bd34ae98
ibra101408 <danikvod@gmail.com>
2023-06-22 18:47:45 +0300

    185462779 WIP oAuth2

--- custom.d.ts
+++ custom.d.ts
@@ -4,11 +4,8 @@ import { WebSocket } from 'ws';
 
 export interface IRequestWithSession extends Request {
     sessionToken?: string;
-    user?: any;
-   // err?: any;
+    user: any;
     session?: any;
-    isGoogleUser?: boolean;
-    logout?: any;
     userId?: number;
     ws: WebSocket; // Add the 'ws' property of type 'WebSocket'
 }
